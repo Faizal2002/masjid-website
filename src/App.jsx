a4fa580fac923ebfaf46c6d237b183eeeae88a64
faizal <faizifaizal2002@gmail.com>
2025-06-24 21:43:46 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './Hero';
 import Header from './Header'
 import About from './About';
 import PrayerTimings from './Prayer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom'
 import Events from './Events';
 import Contact from './Contact';
 function App() {
